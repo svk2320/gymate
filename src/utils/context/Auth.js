7198b44ca7dd38b0328c0313f3fb4ecb455dad93
vasanth2320 <vasanthkumars2025@gmail.com>
2023-11-12 17:26:53 +0530

    changed folder structure

--- src/utils/context/Auth.js
+++ src/utils/context/Auth.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { auth, db } from "../firebase";
+import { auth, db } from "../api/firebase";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
